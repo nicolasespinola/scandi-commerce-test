2ddbbfd4d4b1dc08b045be91d9461bf767a30bc0
nicob <nicolasespinolaxcx@gmail.com>
2022-04-15 13:06:41 -0400

    Add apollo fetching properties to project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import APIcategories from './components/APIcategories';
+
+// apollo client setup
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql'
+});
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello Old Component Class!
-      </div>
+      <ApolloProvider client={client}>
+        <div>
+          Hello Old Component Class!
+                  <APIcategories/>
+        </div>
+      </ApolloProvider>
     );
   }
 }
